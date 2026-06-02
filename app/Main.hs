30b221afa82cb804408915932ce8a735065e01d7
Renatas <renatas.pa@hotmail.com>
2026-06-02 03:53:44 -0300

    configurando conexão com banco via variável de ambiente

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,18 @@ module Main where
 import Network.Wai.Handler.Warp
 import Server.Handler
 import Database.PostgreSQL.Simple
+import System.Environment (lookupEnv)
 
 main :: IO ()
 main = do
-  conn <- connect defaultConnectInfo
-    { connectHost     = "localhost"
-    , connectDatabase = "taskdb"
-    , connectUser     = "postgres"
-    , connectPassword = "postgres"
-    }
+  dbUrl <- lookupEnv "DATABASE_URL"
+  conn <- case dbUrl of
+    Just url -> connectPostgreSQL (fromString url)
+    Nothing  -> connect defaultConnectInfo
+      { connectHost     = "localhost"
+      , connectDatabase = "taskdb"
+      , connectUser     = "postgres"
+      , connectPassword = "postgres"
+      }
   putStrLn "Servidor rodando na porta 8080..."
   run 8080 (app conn)
